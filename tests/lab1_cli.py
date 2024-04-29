ec1ab979b9e78315d911b8926886af0fe52cab9c
CHB-0r1s <borisluzin2004@gmail.com>
2024-04-29 23:45:30 +0300

    Make minor design changes

--- tests/lab1_cli.py
+++ tests/lab1_cli.py
@@ -9,23 +9,23 @@ def dir_from_file(file_path, delimiter: str):
 def erase_meta_inf(row_file: bytes) -> (bytes, bytes):
     splited_row_file: list[bytes] = row_file.split(b"#")
     test_data, erased_meta_inf = b"".join(splited_row_file[:-1]), splited_row_file[-1]
-    print(test_data)
-    print(erased_meta_inf)
     return test_data, erased_meta_inf
 
 
 class Lab1API:
-    # CWD_WIN_BORIS = "C:\\Users\\Борис\\PycharmProjects\\CI-CD_for_CM\\tests\\"
-    CWD_WIN_BORIS = "/home/user/Desktop/CI-CD_for_CM/tests"
+    CWD_WIN_BORIS = "C:\\Users\\Борис\\PycharmProjects\\CI-CD_for_CM\\tests\\"
+    # CWD_WIN_BORIS = "/home/user/Desktop/CI-CD_for_CM/tests"
     CWD_PIPELINE = dir_from_file(__file__, "/")
 
     @staticmethod
     def write_in_console_from_file(command, file_path: str) -> (int, bytes):
-        print(file_path)
         file_with_input = open(file_path, "rb")
         treated_input, meta_inf = erase_meta_inf(file_with_input.read().replace(b"\r", b""))
+        print(treated_input)
         command_and_param: list[str] = shlex.split(command, comments=False, posix=True)
-        echo_command: list[str] = ["echo", treated_input, "|"]
-
-        with Popen(echo_command + command_and_param, stdout=PIPE, stderr=PIPE, cwd=Lab1API.CWD_PIPELINE) as proc:
+        echo_command: list[str] = ["echo", f"\'{treated_input.decode('utf-8')}\'", "|"]
+        with open("t.sh", mode="a") as file:
+            file.write(" ".join(echo_command))
+            file.write(" ".join(command_and_param))
+        with Popen(["sh", "t.sh"], stdout=PIPE, stderr=PIPE, cwd=Lab1API.CWD_PIPELINE) as proc:
             return proc.wait(), meta_inf
--- tests/test_input_format.py
+++ tests/test_input_format.py
@@ -11,11 +11,11 @@ def dir_from_file(file_path, delimiter: str):
     return delimiter.join(file_path.split(delimiter)[:-1])
 
 
-d = "/"
-FIXTURE_DIR_PATH = f'{dir_from_file(__file__, d)}/input_fixtures'
+d = "\\"
+FIXTURE_DIR_PATH = f'{dir_from_file(__file__, d)}\\input_fixtures'
 
 
-@pytest.mark.parametrize("test_file_path, ", [f"{FIXTURE_DIR_PATH}/G_Regular/01_empty_n_input.txt"])
+@pytest.mark.parametrize("test_file_path, ", [f"{FIXTURE_DIR_PATH}\\G_Regular\\01_empty_n_input.txt"])
 def test_empty_input(test_file_path):
     return_code, meta_inf = Lab1API.write_in_console_from_file("python3 main_si.py", test_file_path)
     assert f_Antoha(meta_inf, test_file_path)
