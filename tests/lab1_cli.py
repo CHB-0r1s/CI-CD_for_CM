25dec9c44ef886feb2ce5470fc57f6730ed4e27c
CHB-0r1s <borisluzin2004@gmail.com>
2024-04-29 17:10:41 +0300

    Add metainf eraser for scrap #METAINF

--- tests/lab1_cli.py
+++ tests/lab1_cli.py
@@ -6,8 +6,10 @@ def dir_from_file(file_path, delimiter: str):
     return delimiter.join(file_path.split(delimiter)[:-1])
 
 
-def erase_meta_inf(row_file: bytes) -> bytes:
-    return b"".join(row_file.split(b"#")[:-1])
+def erase_meta_inf(row_file: bytes) -> (bytes, bytes):
+    splited_row_file: list[bytes] = row_file.split(b"#")
+    test_data, erased_meta_inf = splited_row_file[:-1], splited_row_file[-1]
+    return test_data, erased_meta_inf
 
 
 class Lab1API:
@@ -15,12 +17,12 @@ class Lab1API:
     CWD_PIPELINE = dir_from_file(__file__, "/")
 
     @staticmethod
-    def write_in_console_from_file(command, file_path: str) -> int:
+    def write_in_console_from_file(command, file_path: str) -> (int, bytes):
         print(file_path)
         file_with_input = open(file_path, "rb")
-        treated_input: bytes = erase_meta_inf(file_with_input.read().replace(b"\r", b""))
+        treated_input, meta_inf = erase_meta_inf(file_with_input.read().replace(b"\r", b""))
         command_and_param: list[str] = shlex.split(command, comments=False, posix=True)
         echo_command: list[str] = ["echo", treated_input, "|"]
 
         with Popen(echo_command + command_and_param, stdout=PIPE, stderr=PIPE, cwd=Lab1API.CWD_PIPELINE) as proc:
-            return proc.wait()
+            return proc.wait(), meta_inf
