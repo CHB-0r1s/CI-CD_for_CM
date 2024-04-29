50ad58878efd0032a26e70103503c7f47cc8a386
CHB-0r1s <borisluzin2004@gmail.com>
2024-04-29 15:07:42 +0300

    Edit actions

--- tests/test_input_format.py
+++ tests/test_input_format.py
@@ -11,7 +11,7 @@ d = "\\"
 FIXTURE_DIR_PATH = f'{dir_from_file(__file__, d)}\\input_format_fixtures'
 
 
-@pytest.mark.parametrize("file_with_test", [f"{FIXTURE_DIR_PATH}\\01_empty_n_input.txt"])
+@pytest.mark.parametrize("file_with_test", [f"{FIXTURE_DIR_PATH}/01_empty_n_input.txt"])
 def test_empty_input(file_with_test):
     return_code = Lab1API.write_in_console_from_file("python3 main.py", file_with_test)
     assert return_code == 0
