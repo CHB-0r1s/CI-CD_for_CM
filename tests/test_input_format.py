21aeabdb03e32dfed02b0393079eaeb784d650b3
CHB-0r1s <borisluzin2004@gmail.com>
2024-04-29 19:58:05 +0300

    Correct Lab1's (with G_R) output in lab1_test_result.txt D x1 x2 x3 ... xn R1 R2 R3 ... Rn

--- tests/test_input_format.py
+++ tests/test_input_format.py
@@ -3,7 +3,7 @@ import pytest
 from lab1_cli import Lab1API
 
 
-def f_Antoha(a, b, c):
+def f_Antoha(a, b):
     return True
 
 
@@ -17,6 +17,6 @@ FIXTURE_DIR_PATH = f'{dir_from_file(__file__, d)}/input_fixtures'
 
 @pytest.mark.parametrize("test_file_path, ", [f"{FIXTURE_DIR_PATH}/01_empty_n_input.txt"])
 def test_empty_input(test_file_path):
-    return_code, meta_inf, output = Lab1API.write_in_console_from_file("python3 main.py", test_file_path)
-    assert f_Antoha(meta_inf, output, test_file_path)
+    return_code, meta_inf = Lab1API.write_in_console_from_file("python3 main.py", test_file_path)
+    assert f_Antoha(meta_inf, test_file_path)
     assert return_code == 0
