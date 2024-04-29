358c325c3279f2a4842a893fce026b15be3ba580
CHB-0r1s <borisluzin2004@gmail.com>
2024-04-29 15:04:50 +0300

    Edit actions

--- tests/test_input_format.py
+++ tests/test_input_format.py
@@ -13,6 +13,5 @@ FIXTURE_DIR_PATH = f'{dir_from_file(__file__, d)}\\input_format_fixtures'
 
 @pytest.mark.parametrize("file_with_test", [f"{FIXTURE_DIR_PATH}\\01_empty_n_input.txt"])
 def test_empty_input(file_with_test):
-    assert file_with_test == "C:\\Users\\Борис\\PycharmProjects\\CI-CD_for_CM\\tests\\input_format_fixtures\\01_empty_n_input.txt"
     return_code = Lab1API.write_in_console_from_file("python3 main.py", file_with_test)
     assert return_code == 0
