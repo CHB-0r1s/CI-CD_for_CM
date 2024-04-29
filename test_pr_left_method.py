b9eafd216a571e6a846ee05b140f660a36a6f65c
user <anton@zinchenko.pro>
2024-04-29 18:11:02 +0300

    delete trash

--- test_pr_left_method.py
+++
@@ -1,8 +0,0 @@
-from main import pr_left_method
-
-
-def test_pr_left_method_ok():
-    func = lambda x: x**3 - x**2
-    a, b = 0, 2
-    n = 1000
-    assert abs(4/3 - pr_left_method(func, a, b, n)) < 0.1
