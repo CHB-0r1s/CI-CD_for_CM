d0fbfc2782192f52d7f1411689ab029349f386a8
CHB_0r1s <borisluzin2004@gmail.com>
2024-09-07 16:48:41 +0300

    Add building stage

--- docker_draft.py
+++ docker_draft.py
@@ -3,8 +3,12 @@ import docker
 TEST_SUCCESS_STR = "test_input_format.py ......                                              [100%]"
 
 client = docker.from_env()
+client.images.build(path="Dockerfile")
+
 print(client.images.list())
+
 test_result = client.containers.run("my-cicd").decode('utf-8', errors='ignore')
+
 if TEST_SUCCESS_STR in test_result:
     print("OK")
 else:
